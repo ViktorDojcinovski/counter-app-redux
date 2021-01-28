67289f53d303c7ff7d4926f461eb8f1ee182a5df
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2021-01-28 09:35:16 +0100

    Implement action dispatching flow

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,12 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { increaseCounter, decreaseCounter } from "../../redux";
+
 const Counter = (props) => {
-  return <p style={{ fontSize: "40px", margin: "20px" }}>{props.counter}</p>;
+  // step 2: define an event handler which will trigger an action dispatcher
+  const onClickIncrease = () => {
+    // step 3: call the action dispatcher
+    props.increaseCounter();
+  };
+
+  const onClickDecrease = () => {
+    props.decreaseCounter();
+  };
+  return (
+    <>
+      <p style={{ fontSize: "40px", margin: "20px" }}>
+        counter: {props.counter}
+      </p>
+      {/* step 1:  create an event listener which will trigger an event handler*/}
+      <button onClick={onClickDecrease}> - </button>
+      <button onClick={onClickIncrease}> + </button>
+    </>
+  );
 };
 
 const mapStateToProps = (state) => ({
   counter: state.counter,
 });
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => ({
+  // step 4: define the action dispatcher with the help of the dispatch keyword
+  // (step 5 continues in the redux structure)
+  increaseCounter: () => dispatch(increaseCounter()),
+  decreaseCounter: () => dispatch(decreaseCounter()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); // connect()()
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,16 +1,35 @@
 import { createStore } from "redux";
 
-// const increaseCounter = () => ({
-//   type: "INCREASE_COUNTER",
-// });
+// step 5: declare action creator (function that returns an action)
+const increaseCounter = () => {
+  return {
+    type: "INCREASE_COUNTER",
+  };
+};
+
+const decreaseCounter = () => {
+  return {
+    type: "DECREASE_COUNTER",
+  };
+};
+
+// student: {age: integer, name: string, profession: string}
 
 const INITIAL_STATE = {
-  p: "default",
-  counter: 25,
+  counter: 5,
+  students: [],
 };
 
 export const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case "INCREASE_COUNTER":
+      return {
+        counter: state.counter >= 10 ? 10 : state.counter + 1,
+      };
+    case "DECREASE_COUNTER":
+      return {
+        counter: state.counter > 0 ? state.counter - 1 : 0,
+      };
     default:
       return { ...state }; // --> { p: "default, counter: 0 }
       break;
@@ -19,4 +38,4 @@ export const reducer = (state = INITIAL_STATE, action) => {
 
 const store = createStore(reducer);
 
-export { store };
+export { store, increaseCounter, decreaseCounter };
